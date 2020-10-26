f80ba2d840ed73919f7c1a430798320e1bddacd3
Moises Sanabria <m@moises.tech>
2020-10-25 23:11:00 -0400

    ReactDOM, App document.getElementById

---
+++ src/Pages/App/index.js
@@ -0,0 +1,27 @@
+import "./index.css";
+import { useState, useEffect } from "react";
+import Header from '../../components/Header';
+import Form from '../../components/Form';
+import List from '../../components/List';
+
+export default function App() {
+  const [income, setIncome] = useState([])
+  const [totalIncome, setTotalIncome] = useState(0);
+
+  useEffect(() => {
+		let temp = 0;
+		for(let i = 0; i < income.length; i++) {
+			temp += parseInt(income[i].price);
+		}
+
+		setTotalIncome(temp);
+	}, [income]);
+
+  return (
+    <div className="App">
+			<Header totalIncome={totalIncome} />
+			<Form income={income} setIncome={setIncome} />
+			<List income={income} setIncome={setIncome} />
+		</div>
+  )
+}
