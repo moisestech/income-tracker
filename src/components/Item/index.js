a6b426af53bc44d70759648dcc10990b1ef4ceaa
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-26 21:11:49 -0400

    testing InforForm Date clg, removeItem

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,20 +1,24 @@
 import "./item.css";
-export default function Item({ income, index, removeIncome}) {
+export default function Item({ income, index, removeIncome }) {
   let date = new Date(income.date);
   let day = date.getDate();
   let month = date.getMonth() + 1;
   let year = date.getFullYear();
 
-  const removeHandle = i => {
+  const removeHandle = (i) => {
     removeIncome(i);
-  }
+  };
+
+  console.log("IncomeForm DATE:>> ", date);
 
   return (
     <div className="income-item">
-      <button className="remove-item" onClick={() => removeHandle(index)}>x</button>
+      <button className="remove-item" onClick={() => removeHandle(index)}>
+        x
+      </button>
       <div className="desc">{income.desc}</div>
       <div className="price">£{income.price}</div>
       <div className="date">{day + "/" + month + "/" + year}</div>
     </div>
-  )
+  );
 }
