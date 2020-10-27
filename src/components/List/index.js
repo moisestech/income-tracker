18ea9101820270c75ccf80b045952dc73fbee947
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-26 21:12:50 -0400

    form prettier

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,34 +1,32 @@
-import "./list.css"
-import PropTypes from 'prop-types';
-import Item from '../Item';
+import "./list.css";
+import PropTypes from "prop-types";
+import Item from "../Item";
 
 export default function List({ income, setIncome }) {
-  const removeIncome = i => {
+  const removeIncome = (i) => {
     let temp = income.filter((v, index) => index != i);
     setIncome(temp);
-  }
+  };
 
   const sortByDate = (a, b) => {
     return a.date - b.date;
-  }
+  };
 
   return (
     <div className="income-list">
-      {
-        income.sort(sortByDate).map((value, index) => (
-          <Item 
-            key={index} 
-            income={value} 
-            index={index} 
-            removeIncome={removeIncome}
-          />
-        ))
-      }
+      {income.sort(sortByDate).map((value, index) => (
+        <Item
+          key={index}
+          income={value}
+          index={index}
+          removeIncome={removeIncome}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
 List.propTypes = {
   income: PropTypes.array.isRequired,
-  setIncome: PropTypes.func.isRequired
-}
+  setIncome: PropTypes.func.isRequired,
+};
