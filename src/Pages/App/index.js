1511134693234547519999a50b7f696543275ce3
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-26 21:07:53 -0400

    format Prettier useState income, totalIncome, useEffect temp setTotalIncome(temp)

--- src/Pages/App/index.js
+++ src/Pages/App/index.js
@@ -1,27 +1,27 @@
 import "./index.css";
 import { useState, useEffect } from "react";
-import Header from '../../components/Header';
-import Form from '../../components/Form';
-import List from '../../components/List';
+import Header from "../../components/Header";
+import Form from "../../components/Form";
+import List from "../../components/List";
 
 export default function App() {
-  const [income, setIncome] = useState([])
+  const [income, setIncome] = useState([]);
   const [totalIncome, setTotalIncome] = useState(0);
 
   useEffect(() => {
-		let temp = 0;
-		for(let i = 0; i < income.length; i++) {
-			temp += parseInt(income[i].price);
-		}
-
-		setTotalIncome(temp);
-	}, [income]);
+    let temp = 0;
+    for (let i = 0; i < income.length; i++) {
+      temp += parseInt(income[i].price);
+    }
+    console.log("INCOME TEMP: ", temp);
+    setTotalIncome(temp);
+  }, [income]);
 
   return (
     <div className="App">
-			<Header totalIncome={totalIncome} />
-			<Form income={income} setIncome={setIncome} />
-			<List income={income} setIncome={setIncome} />
-		</div>
-  )
+      <Header totalIncome={totalIncome} />
+      <Form income={income} setIncome={setIncome} />
+      <List income={income} setIncome={setIncome} />
+    </div>
+  );
 }
